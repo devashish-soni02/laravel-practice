8f04bbefc972a935fc6e366451381a5ed5a38db8
devsoni34 <devahsishs@kaitensoftware.com>
2023-12-13 00:44:44 +0530

    Dev Laravel Practice

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+@section('content')
+    <section class="container my-5">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <img src="https://img.freepik.com/free-vector/teamwork-concept-landing-page_52683-20165.jpg?w=826&t=st=1702407969~exp=1702408569~hmac=5564980b25d95135ed7465383d913bb0ad39873c92b65ed94d56bfde60887f46"
+                    alt="About Us Image" class="img-fluid rounded">
+            </div>
+            <div class="col-lg-6 mt-4 mt-lg-0">
+                <h2 class="display-4 mb-4">About Us</h2>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod aliquet mauris, non
+                    varius augue lacinia id. Duis dapibus auctor dictum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod aliquet mauris, non varius augue
+                    lacinia id. Duis dapibus auctor dictum. Sed aliquet lacus non nisl fringilla, eu euismod odio cursus.
+                    Integer vel enim nec dui malesuada venenatis.</p>
+            </div>
+        </div>
+    </section>
+@endsection
